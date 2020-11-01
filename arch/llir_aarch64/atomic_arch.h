8cdd6cfaaf39ce6900987abaf9c07c2fd13e8004
Nandor Licker <n@ndor.email>
2020-11-01 22:11:56 +0000

    [llir-musl][aarch64] Atomic ops

--- arch/llir_aarch64/atomic_arch.h
+++ arch/llir_aarch64/atomic_arch.h
@@ -1,41 +1,85 @@
 #define a_ll a_ll
 static inline int a_ll(volatile int *p)
 {
-	__builtin_trap();
+  int v;
+  __asm__ __volatile__
+    ( "aarch64_ll.i32 %0, %1"
+    : "=r"(v)
+    : "r"(p)
+    :
+    );
+  return v;
 }
 
 #define a_sc a_sc
 static inline int a_sc(volatile int *p, int v)
 {
-	__builtin_trap();
+  int r;
+  __asm__ __volatile__
+    ( "aarch64_sc.i32 %0, %1, %2"
+    : "=r"(r)
+    : "r"(p), "r"(v)
+    : "memory"
+    );
+  return !v;
 }
 
 #define a_barrier a_barrier
 static inline void a_barrier()
 {
-	__builtin_trap();
+  __asm__ __volatile__ ("aarch64_dmb" : : : "memory");
 }
 
 #define a_cas a_cas
 static inline int a_cas(volatile int *p, int t, int s)
 {
-	__builtin_trap();
+  int old;
+  do {
+    old = a_ll(p);
+    if (old != t) {
+      a_barrier();
+      break;
+    }
+  } while (!a_sc(p, s));
+  return old;
 }
 
 #define a_ll_p a_ll_p
 static inline void *a_ll_p(volatile void *p)
 {
-	__builtin_trap();
+  void *v;
+  __asm__ __volatile__
+    ( "aarch64_ll.i64 %0, %1"
+    : "=r"(v)
+    : "r"(p)
+    :
+    );
+  return v;
 }
 
 #define a_sc_p a_sc_p
 static inline int a_sc_p(volatile int *p, void *v)
 {
-	__builtin_trap();
+  int r;
+  __asm__ __volatile__
+    ( "aarch64_sc.i32 %0, %1, %2"
+    : "=r"(r)
+    : "r"(p), "r"(v)
+    : "memory"
+    );
+  return !v;
 }
 
 #define a_cas_p a_cas_p
 static inline void *a_cas_p(volatile void *p, void *t, void *s)
 {
-	__builtin_trap();
+  void *old;
+  do {
+    old = a_ll_p(p);
+    if (old != t) {
+      a_barrier();
+      break;
+    }
+  } while (!a_sc_p(p, s));
+  return old;
 }
