430e705d09eb7feee772fca62e7caa9f979550cf
Nandor Licker <n@ndor.email>
2020-11-02 14:40:40 +0000

    [llir-musl] Added missing aarch64 symbol

--- src/signal/llir_aarch64/restore.s
+++ src/signal/llir_aarch64/restore.s
@@ -1,6 +1,15 @@
+  .globl __restore
+  .hidden __restore
+__restore:
+  mov.i64     $0, 139
+  syscall     $0
+  trap
+  .end
+
   .globl __restore_rt
   .hidden __restore_rt
 __restore_rt:
   mov.i64     $0, 139
   syscall     $0
   trap
+  .end
