da77363960dc79dea672406d6d1ef924b9b48062
Nandor Licker <n@ndor.email>
2020-11-11 15:33:34 +0000

    [llir-musl] Added missing alias

--- arch/powerpc64/bits/user.h
+++ arch/powerpc64/bits/user.h
@@ -1,8 +1,60 @@
+struct user_pt_regs {
+	unsigned long gpr[32];
+	unsigned long nip;
+	unsigned long msr;
+	unsigned long orig_gpr3;
+	unsigned long ctr;
+	unsigned long link;
+	unsigned long xer;
+	unsigned long ccr;
+	unsigned long softe;
+	unsigned long trap;
+	unsigned long dar;
+	unsigned long dsisr;
+	unsigned long result;
+};
+
+struct pt_regs {
+	union {
+		struct user_pt_regs user_regs;
+		struct {
+			unsigned long gpr[32];
+			unsigned long nip;
+			unsigned long msr;
+			unsigned long orig_gpr3;
+			unsigned long ctr;
+			unsigned long link;
+			unsigned long xer;
+			unsigned long ccr;
+			unsigned long softe;
+			unsigned long trap;
+			unsigned long dar;
+			unsigned long dsisr;
+			unsigned long result;
+		};
+	};
+
+	union {
+		struct {
+			unsigned long ppr;
+			unsigned long kuap;
+		};
+		unsigned long __pad[2];
+	};
+};
+
+struct user_fpu_struct {
+  unsigned long fp_regs[16];
+  unsigned long xfp_regs[16];
+  unsigned long fpscr;
+  unsigned long fpul;
+};
+typedef struct user_fpu_struct elf_fpregset_t;
+
 struct user {
-	struct {
-		unsigned long gpr[32], nip, msr, orig_gpr3, ctr, link, xer, ccr, softe;
-		unsigned long trap, dar, dsisr, result;
-	} regs;
+  struct pt_regs regs;
+  struct user_fpu_struct fpu;
+  int u_fpvalid;
 	unsigned long u_tsize, u_dsize, u_ssize;
 	unsigned long start_code, start_data, start_stack;
 	long signal;
@@ -15,7 +67,6 @@ struct user {
 #define ELF_NFPREG 33
 #define ELF_NVRREG 34
 typedef unsigned long elf_greg_t, elf_gregset_t[ELF_NGREG];
-typedef double elf_fpreg_t, elf_fpregset_t[ELF_NFPREG];
 typedef struct { unsigned u[4]; }
 #ifdef __GNUC__
 __attribute__((__aligned__(16)))
