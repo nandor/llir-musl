f62406ddecdeb7cfd2b1fe51861299365632ddb5
Nandor Licker <n@ndor.email>
2020-12-24 17:37:55 +0000

    [llir-musl] Added setjmp/longjmp aliases.

--- src/setjmp/llir_aarch64/longjmp.s
+++ src/setjmp/llir_aarch64/longjmp.s
@@ -21,3 +21,26 @@ longjmp:
   ld.i64              $9, [$8]
 
   raise.setjmp        $9, $6, $5
+
+  .section .text
+  .globl _longjmp
+_longjmp:
+  .noinline
+  .args               i64, i64
+  .call               c
+
+  arg.i64             $0, 0
+  arg.i64             $1, 1
+
+  mov.i64             $2, 0
+  mov.i64             $3, 1
+  select.i64          $4, $1, $2, $3
+  add.i64             $5, $1, $4
+
+  ld.i64              $6, [$0]
+
+  mov.i64             $7, 8
+  add.i64             $8, $0, $7
+  ld.i64              $9, [$8]
+
+  raise.setjmp        $9, $6, $5
--- src/setjmp/llir_x86_64/setjmp.s
+++ src/setjmp/llir_x86_64/setjmp.s
@@ -17,3 +17,22 @@ setjmp:
 
   mov.i32     $5, 0
   ret         $5
+
+  .section .text
+  .globl _setjmp
+_setjmp:
+  .call       setjmp
+  .args       i64
+
+  arg.i64     $0, 0
+
+  get.i64     $1, $frame_addr
+  st          [$0], $1
+
+  mov.i64     $2, 8
+  add.i64     $3, $0, $2
+  get.i64     $4, $ret_addr
+  st          [$3], $4
+
+  mov.i32     $5, 0
+  ret         $5
