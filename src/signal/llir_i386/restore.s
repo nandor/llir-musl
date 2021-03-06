3f6610508e26234c8a1ad5c9f886a487f82ff79a
Nandor Licker <n@ndor.email>
2021-03-06 17:57:34 +0200

    [llir-musl] Restore stub

--- src/signal/llir_i386/restore.s
+++ src/signal/llir_i386/restore.s
@@ -1,6 +1,15 @@
+  .globl __restore
+  .hidden __restore
+__restore:
+  mov.i32     $0, 119
+  syscall     $0
+  trap
+  .end
+
   .globl __restore_rt
   .hidden __restore_rt
 __restore_rt:
-  mov.i32     $0, 15
+  mov.i32     $0, 173
   syscall     $0
   trap
+  .end
